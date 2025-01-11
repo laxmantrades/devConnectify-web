b41c4ed8aa2a2bf8c6e1e0ecc1aac73e3ce383b2
Laxman Giri <laxmanvatij@gmail.com>
2025-01-11 21:24:34 +0100

    projectsapi

---
+++ src/Features/projectApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../utils/constants";
+
+export const projectApi = createApi({
+  reducerPath: "projectApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    fetchProject: builder.query({
+      query: (projectId) => ({
+        url: `project/view/${projectId}`,
+        method: "get",
+      }),
+    }),
+    loadPersonalProjects: builder.query({
+      query: () => ({ url: "myprojects", method: "GET" }),
+    }),
+  }),
+});
+export const { useFetchProjectQuery, useLoadPersonalProjectsQuery } =
+  projectApi;
